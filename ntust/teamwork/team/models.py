5ddb3812df66b98b55398e42c5275a2c139cf732
TZU-HSUAN YEH <stupidkids201@gmail.com>
2019-03-08 12:48:34 +0800

    新增mysql

--- ntust/teamwork/team/models.py
+++ ntust/teamwork/team/models.py
@@ -8,29 +8,38 @@
 from django.db import models
 from django_mysql.models import Bit1BooleanField
 
-class Activity(models.Model):
-    activity_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=60)
-    pia_manager_name = models.CharField(max_length=20)
-    pia_manager_email = models.CharField(max_length=35)
-    activity_manager_name = models.CharField(max_length=20)
-    activity_manager_email = models.CharField(max_length=35)
-    date = models.DateTimeField()
-    description = models.CharField(max_length=512)
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True) #用戶id
+    account = models.CharField(max_length=20) #用戶帳號
+    password = models.CharField(max_length=20) #用戶密碼
+    permission_level = models.IntegerField() #用戶權限等級
+    name = models.CharField(max_length=20) #用戶姓名
+    activity_id = models.IntegerField() #用戶參與之專案id
 
     class Meta:
-        managed = False
+        db_table = 'user'
+
+class Activity(models.Model): #pia專案簡介表
+    activity_id = models.AutoField(primary_key=True) #pia專案id,pk
+    name = models.CharField(max_length=60) #pia專案名字
+    pia_manager_name = models.CharField(max_length=20) #pia專案負責人姓名
+    pia_manager_email = models.CharField(max_length=35) #pia專案負責人email
+    activity_manager_name = models.CharField(max_length=20) #系統負責人姓名
+    activity_manager_email = models.CharField(max_length=35) #系統負責人email
+    date = models.DateTimeField() #pia專案日期
+    description = models.CharField(max_length=512) #pia專案簡短描述
+
+    class Meta:
         db_table = 'activity'
 
 
-class Questionary1Answer(models.Model):
-    answer_id = models.AutoField(primary_key=True)
-    questionary_id = models.IntegerField()
-    questionary_ans = models.CharField(max_length=512)
-    activity_id = models.CharField(max_length=512)
+class Questionary1Answer(models.Model): #問卷第一大題答案表
+    answer_id = models.AutoField(primary_key=True) #個別答案id
+    questionary_id = models.IntegerField() #問卷id,對應至問卷第一大題題目表第n小題
+    questionary_ans = models.CharField(max_length=512) #答案內容
+    activity_id = models.CharField(max_length=512) #專案id
 
     class Meta:
-        managed = False
         db_table = 'questionary1_answer'
 
 
@@ -39,7 +48,6 @@ class Questionary1Template(models.Model):
     questionary_description = models.CharField(max_length=512)
 
     class Meta:
-        managed = False
         db_table = 'questionary1_template'
 
 
@@ -50,16 +58,14 @@ class Questionary2Answer(models.Model):
     activity_id = models.CharField(max_length=512)
 
     class Meta:
-        managed = False
         db_table = 'questionary2_answer'
 
 
 class Questionary2Template(models.Model):
-    questionary_id = models.IntegerField()
+    questionary_id = models.IntegerField(primary_key=True)
     questionary_description = models.CharField(max_length=512)
 
     class Meta:
-        managed = False
         db_table = 'questionary2_template'
 
 
@@ -70,16 +76,14 @@ class Questionary3Answer(models.Model):
     activity_id = models.CharField(max_length=512)
 
     class Meta:
-        managed = False
         db_table = 'questionary3_answer'
 
 
 class Questionary3Template(models.Model):
-    questionary_id = models.IntegerField()
+    questionary_id = models.IntegerField(primary_key=True)
     questionary_description = models.CharField(max_length=512)
 
     class Meta:
-        managed = False
         db_table = 'questionary3_template'
 
 
@@ -90,27 +94,14 @@ class Questionary4Answer(models.Model):
     activity_id = models.CharField(max_length=512)
 
     class Meta:
-        managed = False
         db_table = 'questionary4_answer'
 
 
 class Questionary4Template(models.Model):
-    questionary_id = models.IntegerField()
+    questionary_id = models.IntegerField(primary_key=True)
     questionary_description = models.CharField(max_length=512)
 
     class Meta:
-        managed = False
         db_table = 'questionary4_template'
 
 
-class User(models.Model):
-    user_id = models.AutoField(primary_key=True)
-    account = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
-    permission_level = models.IntegerField()
-    name = models.CharField(max_length=20)
-    activity_id = models.IntegerField()
-
-    class Meta:
-        managed = False
-        db_table = 'user'
--- ntust/teamwork/team/views.py
+++ ntust/teamwork/team/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render_to_response
-from django.http import HttpResponse
-
+from django.shortcuts import render,render_to_response, get_object_or_404
+from django.template import RequestContext 
+from django.http import HttpResponseRedirect
+from . import models
+from .models import User
 # Create your views here.
 def index(request):
-	return render_to_response('team/home.html',locals())
+    return render_to_response('team/home.html',locals())
+
+def login(request):
+    account = request.POST['account']
+    password = request.POST['password']
+    user = models.User.objects.filter(account=account,password=password)
+    if len(user)==1:
+        return render_to_response('team/choose_pia.html',locals())
+    else:
+        return render_to_response('team/home.html',locals())
