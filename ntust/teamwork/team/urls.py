a927cca3e7ac520ab8fd1652f75554e48c7b0183
jocelyn0326 <37055239+jocelyn0326@users.noreply.github.com>
2019-03-14 16:12:08 +0800

    add new feature to questionary function.

--- ntust/teamwork/team/urls.py
+++ ntust/teamwork/team/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^login/$', views.login, name='login'),
-	url(r'^overview/$', views.show_qn1, name='show_qn1'),
+	url(r'^questionary/(?P<questionary_type_id>[-\d]+)$', views.questionary, name='questionary'),
 	url(r'^new_pia/',views.new, name='new_pia'),
 	url(r'^sign/',views.sign, name='sign'),
 	url(r'^home/',views.home, name='home'),
--- ntust/teamwork/team/views.py
+++ ntust/teamwork/team/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.template import RequestContext 
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from . import models
 from .models import User
-from .models import Question
+from .models import Question,QuestionaryType,Answer
+import pdb
 # Create your views here.
 def index(request):
     return render(request,'team/home.html',locals())
@@ -16,10 +17,43 @@ def login(request):
         return render('team/choose_pia.html',locals())
     else:
         return render('team/home.html',locals())
-def show_qn1(request):
-	qn1_list = Question.objects.filter(questionary_type_id=1)
-	path = request.path
-	return render(request,'team/overview.html', locals())
+
+def questionary(request, questionary_type_id=None):
+
+    if questionary_type_id is not None and int(questionary_type_id) in (1, 2, 3):
+        qn_list = Question.objects.filter(questionary_type_id=questionary_type_id)
+
+    else:
+        return HttpResponse('wrong url.')
+
+    if request.method == "POST": #如果表單送出(POST)
+
+        #初始化LIST變數
+        answer_list = list()
+        #queryset是django的objects的型別，跟list一樣，Object[0]，這個就是Model的Object，
+        #locals()代表該funtion所夾帶的所有變數，
+        for i in range(len(qn_list)): 
+            answer_list.append(request.POST.get('answer'+ str(i+1))) 
+            # 取得 input name="answer1~10" 的 value 並加入到 answer_list
+
+        for i in range(len(qn_list)):
+
+            if answer_list[i] != '':
+
+                Answer.objects.create(
+                    question_id=i+1,
+                    context=answer_list[i],
+                    activity_id=1 #test id 
+                )
+
+        '''
+        如果要在這裡停，以上的變數都會被記錄，執行到這行，CMD裡就會停
+        停下後就可以在CMD裡面做運算(ctrl+z可以取消該次請求，輸入c(等於continue的意思)會繼續執行)
+        '''
+        
+
+    return render(request,'team/questionary.html', locals()) #第三個變數代表會傳給第二個變數規定的html的變數有哪些
+
 def new(request):
 	return render(request,'team/new_pia.html', locals())
 def sign(request):
